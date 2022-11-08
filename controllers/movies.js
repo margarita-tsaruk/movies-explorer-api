bbe8493f8f3344dc0108e120a85e3dc1ff5184c2
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-11-08 21:29:53 +0100

    diploma: backend - fixed deleteMovie controller

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,6 +2,7 @@ const Movie = require('../models/movie');
 const ErrorBadReq = require('../errors/errorBadReq');
 const ErrorReqNotFound = require('../errors/errorReqNotFound');
 const ErrorForbiddenReq = require('../errors/errorForbiddenReq');
+
 const {
   messageNoRights,
   messageIncorrectData,
@@ -32,17 +33,18 @@ module.exports.deleteMovie = (req, res, next) => {
   const userId = req.user._id;
   const { movieId } = req.params;
 
-  Movie.findById(movieId)
+  Movie.findOne({ movieId })
     .then((movie) => {
       if (!movie) {
         throw new ErrorReqNotFound(messageNonExistingId);
       }
+
       if (movie.owner.toString() !== userId) {
         throw new ErrorForbiddenReq(messageNoRights);
       } else {
-        return movie.remove()
-          .then(() => {
-            res.send({ message: movie });
+        Movie.findOneAndRemove({ movieId })
+          .then((removedCard) => {
+            res.send(removedCard);
           })
           .catch(next);
       }
--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -4,7 +4,7 @@ const { messageLimiter } = require('../errors/errorsMessages');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   handler: (req, res, next) => next(new ConflictError(messageLimiter)),
 });
 
